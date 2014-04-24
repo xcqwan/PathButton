b59f0c276df5bbd812356dbedd4b5ac2f38e60e9
xcqwan <xcqwan@gmail.com>
2014-04-24 10:24:13 +0800

    add center attr

---
+++ src/com/zombie/wonhot/LayoutConstants.java
@@ -0,0 +1,22 @@
+package com.zombie.wonhot;
+
+public class LayoutConstants {
+	//右下角
+	public static final int RIGHTBOTTOM = 1;
+	//中下
+	public static final int CENTERBOTTOM = 2;
+	//左下角
+	public static final int LEFTBOTTOM = 3;
+	//左中
+	public static final int LEFTCENTER = 4;
+	//左上角
+	public static final int LEFTTOP = 5;
+	//中上
+	public static final int CENTERTOP = 6;
+	//右上角
+	public static final int RIGHTTOP = 7;
+	//右中
+	public static final int RIGHTCENTER = 8;
+	//中间
+	public static final int CENTER = 9;
+}
